0c8bddb2391eb33cf72ee5bf706c676c353f084c
Jbrown95 <browningjustin95@gmail.com>
2018-12-02 17:34:07 -0500

    created user route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+
+  res.json([{
+   id: 1,
+   username: "Jbrow1"
+  }, {
+    id: 2,
+    username: "Jbrow2"
+  }]);
 });
 
 module.exports = router;
